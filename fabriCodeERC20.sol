76ba3603372f46691f2dabfe8b6e629f044ef95c
Fabrizio Fanini <fabrifanini@gmail.com>
2024-12-25 20:52:33 -0300

    update the code

--- fabriCodeERC20.sol
+++ fabriCodeERC20.sol
@@ -3,8 +3,8 @@
 
 pragma solidity ^0.8.20;
 
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract fabriCodeERC20 is ERC20, Ownable {
 
